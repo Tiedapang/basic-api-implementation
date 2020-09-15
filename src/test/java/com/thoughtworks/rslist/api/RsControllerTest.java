2bb4cbe01fb103e8191933f9bff498e05af8bb11
Tiedapang <2750428832@qq.com>
2020-09-15 19:16:57 +0800

    thj add routing test

---
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.rslist.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+@SpringBootTest
+@AutoConfigureMockMvc
+class RsControllerTest {
+    @Autowired
+    MockMvc mocMvc ;
+    @Test
+    public void should_get_rs_event_list() throws Exception{
+        mocMvc.perform(get("/rs/list")).andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]")).andExpect(status().isOk());
+    }
+
+
+}
