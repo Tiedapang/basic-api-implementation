29887ce19d585940a332e2c6becd3a9a35bb2999
Tiedapang <2750428832@qq.com>
2020-09-16 22:55:19 +0800

    set all return is ResponseEntity

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,79 @@
 package com.thoughtworks.rslist.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.domain.RsEvent;
+import com.thoughtworks.rslist.domain.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private List<RsEvent> rsList =initRsEventList();
+  UserController userController = new UserController();
+  private List<RsEvent> initRsEventList() {
+    User user = new User("xiaowang","female",19,"a@thoughtworks.com","18888888888");
+    List<RsEvent> rsEventList = new ArrayList<>();
+    rsEventList.add(new RsEvent("猪肉涨价了","经济",user));
+    rsEventList.add(new RsEvent("小学生放假了","社会时事",user));
+    rsEventList.add(new RsEvent("特朗普辞职了","政治",user));
+    return rsEventList;
+  }
+
+
+  @GetMapping("/rs/{index}")
+  public ResponseEntity getOneRSEvent(@PathVariable int index){
+
+    return ResponseEntity.ok(rsList.get(index - 1));
+  }
+  @GetMapping("/rs/list")
+  public ResponseEntity getList(@RequestParam(required = false) Integer start,@RequestParam(required = false) Integer end){
+    if(start == null || end == null){
+      return ResponseEntity.ok(rsList);
+    }
+    return ResponseEntity.ok(rsList.subList(start - 1,end));
+  }
+  @PostMapping("/rs/addEvent")
+  public ResponseEntity addRsEvent(@RequestBody @Valid RsEvent  rsEvent) throws JsonProcessingException {
+    User user = rsEvent.getUser();
+    if(isExistUserName(user)){
+      rsList.add(rsEvent);
+    }else{
+      userController.addUser(user);
+      rsList.add(rsEvent);
+    }
+  return ResponseEntity.created(null).header("index",String.valueOf(rsList.size()-1)).build();
+
+  }
+
+  private boolean isExistUserName(User user) {
+    return userController.userList.contains(user);
+  }
+
+  @DeleteMapping("/rs/event")
+  public ResponseEntity deleteRsEvent(@RequestBody String  deleteID) throws JsonProcessingException {
+    rsList.remove(Integer.parseInt(deleteID)-1);
+    return ResponseEntity.ok().build();
+  }
+
+  @PostMapping("/rs/updateEvent")
+  public ResponseEntity updateRsEvent(@RequestBody Map params) throws JsonProcessingException {
+    int id = (int) params.get("id");
+    String eventName = (String) params.get("eventName");
+    if(eventName!=null){
+      rsList.get(id-1).setEventName(eventName);
+    }
+    String keyWord = (String) params.get("keyWord");
+    if(keyWord!=null){
+      rsList.get(id-1).setKeyWord(keyWord);
+    }
+    return ResponseEntity.created(null).header("index",String.valueOf(id)).build();
+  }
+
 }
---
+++ src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
@@ -0,0 +1,90 @@
+package com.thoughtworks.rslist.api;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.domain.User;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class UserControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @Order(1)
+    @Test
+    public void should_register_user() throws Exception {
+        User user = new User("thj","female",20,"abc@163.com","1234567891");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+
+
+        mockMvc.perform(get("/user"))
+                .andExpect(jsonPath("$",hasSize(1)))
+                .andExpect(jsonPath("$[0].name",is("thj")))
+                .andExpect(jsonPath("$[0].gender",is("female")))
+                .andExpect(jsonPath("$[0].age",is(20)))
+                .andExpect(jsonPath("$[0].email",is("abc@163.com")))
+                .andExpect(jsonPath("$[0].phone",is("1234567891")))
+                .andExpect(status().isOk());
+    }
+    @Order(2)
+    @Test
+    public void name_shoule_less_than_8() throws Exception {
+        User user = new User("tiehuijie1","female",20,"abc@163.com","1234567891");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+    @Order(3)
+    @Test
+    public void age_shoule_between_18_and_100() throws Exception {
+        User user = new User("thj","female",13,"abc@163.com","1234567891");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Order(4)
+    @Test
+    public void email_shoule_suit_fomate() throws Exception {
+        User user = new User("thj","female",20,"abc163.com","1234567891");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+    @Order(5)
+    @Test
+    public void phone_shoule_suit_fomate() throws Exception {
+        User user = new User("thj","female",20,"abc@163.com","1567891");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+}
