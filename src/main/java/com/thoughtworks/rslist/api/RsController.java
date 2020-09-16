193af96c9f7f236cb55dfaf07623072a08345756
Tiedapang <2750428832@qq.com>
2020-09-16 21:45:34 +0800

    add user validation on RsEvent

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,74 @@
 package com.thoughtworks.rslist.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.domain.RsEvent;
+import com.thoughtworks.rslist.domain.User;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private List<RsEvent> rsList =initRsEventList();
+  UserController userController = new UserController();
+  private List<RsEvent> initRsEventList() {
+    User user = new User("xiaowang","female",19,"a@thoughtworks.com","18888888888");
+    List<RsEvent> rsEventList = new ArrayList<>();
+    rsEventList.add(new RsEvent("猪肉涨价了","经济",user));
+    rsEventList.add(new RsEvent("小学生放假了","社会时事",user));
+    rsEventList.add(new RsEvent("特朗普辞职了","政治",user));
+    return rsEventList;
+  }
+
+
+  @GetMapping("/rs/{index}")
+  public RsEvent getOneRSEvent(@PathVariable int index){
+    return rsList.get(index - 1);
+  }
+  @GetMapping("/rs/list")
+  public List<RsEvent> getList(@RequestParam(required = false) Integer start,@RequestParam(required = false) Integer end){
+    if(start == null || end == null){
+      return rsList;
+    }
+    return rsList.subList(start - 1,end);
+  }
+  @PostMapping("/rs/addEvent")
+  public void addRsEvent(@RequestBody @Valid RsEvent  rsEvent) throws JsonProcessingException {
+    User user = rsEvent.getUser();
+    if(isExistUserName(user)){
+      rsList.add(rsEvent);
+    }else{
+      userController.addUser(user);
+      rsList.add(rsEvent);
+    }
+
+
+  }
+
+  private boolean isExistUserName(User user) {
+    return userController.userList.contains(user);
+  }
+
+  @PostMapping("/rs/deleteEvent")
+  public void deleteRsEvent(@RequestBody String  deleteID) throws JsonProcessingException {
+    rsList.remove(Integer.parseInt(deleteID)-1);
+  }
+
+  @PostMapping("/rs/updateEvent")
+  public void updateRsEvent(@RequestBody Map params) throws JsonProcessingException {
+    int id = (int) params.get("id");
+    String eventName = (String) params.get("eventName");
+    if(eventName!=null){
+      rsList.get(id-1).setEventName(eventName);
+    }
+    String keyWord = (String) params.get("keyWord");
+    if(keyWord!=null){
+      rsList.get(id-1).setKeyWord(keyWord);
+    }
+  }
+
 }
---
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -0,0 +1,121 @@
+package com.thoughtworks.rslist.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.domain.RsEvent;
+import com.thoughtworks.rslist.domain.User;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class RsControllerTest {
+    @Autowired
+    MockMvc mocMvc ;
+
+    @Test
+    public void should_get_one_rs_event() throws Exception {
+        mocMvc.perform(get("/rs/1"))
+                .andExpect(jsonPath("$.eventName",is("猪肉涨价了")))
+                .andExpect(jsonPath("$.keyWord",is("经济")))
+                .andExpect(status().isOk());
+        mocMvc.perform(get("/rs/2"))
+                .andExpect(jsonPath("$.eventName",is("小学生放假了")))
+                .andExpect(jsonPath("$.keyWord",is("社会时事")))
+                .andExpect(status().isOk());
+        mocMvc.perform(get("/rs/3"))
+                .andExpect(jsonPath("$.eventName",is("特朗普辞职了")))
+                .andExpect(jsonPath("$.keyWord",is("政治")))
+                .andExpect(status().isOk());
+    }
+    @Test
+    public void should_get_rs_event_between() throws Exception {
+        mocMvc.perform(get("/rs/list?start=1&end=2"))
+                .andExpect(jsonPath("$",hasSize(2)))
+                .andExpect(jsonPath("$[0].eventName",is("猪肉涨价了")))
+                .andExpect(jsonPath("$[0].keyWord",is("经济")))
+                .andExpect(jsonPath("$[1].eventName",is("小学生放假了")))
+                .andExpect(jsonPath("$[1].keyWord",is("社会时事")))
+                .andExpect(status().isOk());
+    }
+    @Test
+    public void should_add_rs_event() throws Exception {
+        User user = new User("xiaowang","female",19,"a@thoughtworks.com","18888888888");
+        RsEvent rsEvent = new RsEvent("股市跌了","财经",user);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(rsEvent);
+        mocMvc.perform(post("/rs/addEvent").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mocMvc.perform(get("/rs/list"))
+                .andExpect(jsonPath("$",hasSize(4)))
+                .andExpect(jsonPath("$[0].eventName",is("猪肉涨价了")))
+                .andExpect(jsonPath("$[0].keyWord",is("经济")))
+                .andExpect(jsonPath("$[1].eventName",is("小学生放假了")))
+                .andExpect(jsonPath("$[1].keyWord",is("社会时事")))
+                .andExpect(jsonPath("$[2].eventName",is("特朗普辞职了")))
+                .andExpect(jsonPath("$[2].keyWord",is("政治")))
+                .andExpect(jsonPath("$[3].eventName",is("股市跌了")))
+                .andExpect(jsonPath("$[3].keyWord",is("财经")))
+                .andExpect(status().isOk());
+    }
+    @Test
+    public void should_delete_rs_event_by_id() throws Exception {
+        String deleteId = "1" ;
+        mocMvc.perform(post("/rs/deleteEvent").content(deleteId))
+                .andExpect(status().isOk());
+        mocMvc.perform(get("/rs/list"))
+                .andExpect(jsonPath("$",hasSize(2)))
+                .andExpect(jsonPath("$[0].eventName",is("小学生放假了")))
+                .andExpect(jsonPath("$[0].keyWord",is("社会时事")))
+                .andExpect(jsonPath("$[1].eventName",is("特朗普辞职了")))
+                .andExpect(jsonPath("$[1].keyWord",is("政治")))
+                .andExpect(status().isOk());
+    }
+    @Test
+    public void should_update_eventList_by_conditions() throws Exception {
+        JSONObject jsonString = new JSONObject();
+        jsonString.put("id",1);
+        jsonString.put("eventName","这是修改后的名称");
+        jsonString.put("keyWord","修改");
+        mocMvc.perform(post("/rs/updateEvent").content(jsonString.toString()).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mocMvc.perform(get("/rs/list"))
+                .andExpect(jsonPath("$",hasSize(3)))
+                .andExpect(jsonPath("$[0].eventName",is("这是修改后的名称")))
+                .andExpect(jsonPath("$[0].keyWord",is("修改")))
+                .andExpect(jsonPath("$[1].eventName",is("小学生放假了")))
+                .andExpect(jsonPath("$[1].keyWord",is("社会时事")))
+                .andExpect(jsonPath("$[2].eventName",is("特朗普辞职了")))
+                .andExpect(jsonPath("$[2].keyWord",is("政治")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void should_update_eventList_by_only_change_eventName() throws Exception {
+        JSONObject jsonString = new JSONObject();
+        jsonString.put("id",1);
+        jsonString.put("eventName","这是修改后的名称");
+        mocMvc.perform(post("/rs/updateEvent").content(jsonString.toString()).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mocMvc.perform(get("/rs/list"))
+                .andExpect(jsonPath("$",hasSize(3)))
+                .andExpect(jsonPath("$[0].eventName",is("这是修改后的名称")))
+                .andExpect(jsonPath("$[0].keyWord",is("经济")))
+                .andExpect(jsonPath("$[1].eventName",is("小学生放假了")))
+                .andExpect(jsonPath("$[1].keyWord",is("社会时事")))
+                .andExpect(jsonPath("$[2].eventName",is("特朗普辞职了")))
+                .andExpect(jsonPath("$[2].keyWord",is("政治")))
+                .andExpect(status().isOk());
+    }
+}
